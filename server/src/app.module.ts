9dd6081d767019d180d22398bbe1553b52bbb1d6
Shomalik Davlatov <shomalikdavlatov@gmail.com>
2025-08-05 15:18:47 +0500

    Folder structure is created

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { CategoryModule } from './modules/category/category.module';
+import { TrackModule } from './modules/track/track.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, CategoryModule, TrackModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
