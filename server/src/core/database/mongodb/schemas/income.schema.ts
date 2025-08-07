2562b722eb9da038cbc189681099494bb777ecf9
Shomalik Davlatov <shomalikdavlatov@gmail.com>
2025-08-07 11:01:05 +0500

    database changed

--- server/src/core/database/mongodb/schemas/income.schema.ts
+++ server/src/core/database/mongodb/schemas/income.schema.ts
@@ -1,8 +1,13 @@
 import mongoose from 'mongoose';
 
-export const IncomeSchema = new mongoose.Schema({
-    description: { type: String },
-    amount: { type: Number, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, required: true },
-    category: { type: mongoose.Schema.Types.ObjectId, required: true },
-});
+export const IncomeSchema = new mongoose.Schema(
+    {
+        description: { type: String },
+        amount: { type: Number, required: true },
+        user: { type: mongoose.Schema.Types.ObjectId, required: true },
+        category: { type: mongoose.Schema.Types.ObjectId, required: true },
+    },
+    {
+        timestamps: true,
+    },
+);
--- server/src/core/database/mongodb/schemas/user.schema.ts
+++ server/src/core/database/mongodb/schemas/user.schema.ts
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
 
-export const UserSchema = new mongoose.Schema({
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    balance: { type: Number, required: true, default: 0 },
-});
+export const UserSchema = new mongoose.Schema(
+    {
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        balance: { type: Number, required: true, default: 0 },
+    },
+    {
+        timestamps: true,
+    },
+);
